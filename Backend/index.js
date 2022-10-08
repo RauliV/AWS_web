e5b5fc43aaacb58de5a7743c804aa7b391031878
Rauli Virtanen <rauli.virtanen@tuni.fi>
2022-10-08 20:51:10 +0300

    Fixed random bugs and improved the log

--- Backend/index.js
+++ Backend/index.js
@@ -10,16 +10,18 @@ async function addLogLine(userId =' -', description = '-'){
 
   const logFile = './logs/actionlog.txt';
   let content = '';
+  let nextLineNumber = 0;
 
   try {
     content =  await fs.readFile(logFile);  
   } catch(err) {      
     if (err.code == 'ENOENT'){  //File not found
       try {
-        const header = 'LINE' + '\t' + 'DATE AND TIME OF THE ACTION\t' + '\t\t' + userId + 
-        '\t\t\t' + 'CALLER FUNCTION' + '\t\t\t' + 'DESCRIPTION' + '\n';
-        fs.appendFile(logFile, header);  //Create a new logfile
-        await addLogLine('Mr. Bob', 'No file yet, creating one.'); //Logging creating new file
+        const header = 'LINE\tDATE AND TIME OF THE ACTION\tUSERID\t\tCALLER FUNCTION\t\tDESCRIPTION\n';
+        await fs.appendFile(logFile, header);  //Create a new logfile
+        await addLogLine('Mr. Bob', 'No logfile yet, creating one.'); //Logging creating new file
+        console.log('File: ' + logFile + ' not found. I will create one for you');
+        nextLineNumber++;
       } 
       catch (errCreating){
         console.error(errCreating);
@@ -27,19 +29,18 @@ async function addLogLine(userId =' -', description = '-'){
     }
   }
 
-  userId = userId.substring(0,10).padEnd(10)
+  userId = userId.substring(0,8).padEnd(8)
   const caller = callerId.getData(addLogLine);
   const lineNumber = caller.lineNumber;
   const callerFunction = caller.functionName.substring(0,15).padEnd(15) + '(' + lineNumber + ')';
   const timeStamp = new Date().toUTCString();
-  let nextLineNumber = 0;
   const lines = content.toString().split('\n');
   if (lines.length > 2) {     
     const lastLine = lines[lines.length-1]; 
-    nextLineNumber = (Number(lastLine.split('\t\t')[0])) + 1;
+    nextLineNumber = (Number(lastLine.split('\t')[0])) + 1;
   }
-  const newLog = '\n' + nextLineNumber + '\t\t' + timeStamp + '\t\t' + userId + 
-  '\t\t' + callerFunction + '\t\t' + description;
+  const newLog = '\n' + nextLineNumber + '\t' + timeStamp + '\t' + userId + 
+  '\t' + callerFunction + '\t' + description;
   await fs.appendFile(logFile, newLog, function(error) {});  
   
 }
