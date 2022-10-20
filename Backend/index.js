9db95b2363dabe6e1ff3c1d2a48203ee8ab7a594
Onni Hartikainen <onni.hartikainen@tuni.fi>
2022-10-20 17:54:31 +0300

    Add url and skeleton for request triggering build action

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { templateData } from './github.js'
+import fetch from 'node-fetch'
 
 const app = express()
 const port = 8080
@@ -21,6 +22,21 @@ app.post('/api/build', async (req, res) => {
   const json = req.body;
   const packageName = json.package;
   const packageParams = json.parameters;
+  //url for triggering action
+  let gitUrl = "https://api.github.com/repos/PROJ-A2022-G06-AWS-2-Cloud-Organization/PROJ-A2022-G06-AWS-2-Cloud/actions/workflows/github-actions-aws-cdk-deploy.yml/dispatches";
+  /*
+  options = {
+    method: "post",
+    headers: {},
+    ref: packageName,
+    inputs: {
+       AWS_ACCESS_KEY_ID: "",
+       AWS_SECRET_ACCESS_KEY: "",
+       AWS_REGION: ""
+    }
+  }
+  let response = await fetch(gitUrl, options); 
+  */
 
   res.status(200)
   res.setHeader('Content-Type', 'application/json');
