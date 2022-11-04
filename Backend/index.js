465222aaed19ca04ea02d0b7e3d0da77ff9d83b1
Onni Hartikainen <onni.hartikainen@tuni.fi>
2022-11-04 18:03:06 +0200

    Getting latest workflow and jobs

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { addLogLine } from './log.js';
 import { gitFactory } from './github.js';
 import 'node-fetch';
+import fetch from 'node-fetch';
 
 if (!process.env.AWS_GIT_TOKEN) {
   dotenv.config();
@@ -12,7 +13,8 @@ if (!process.env.AWS_GIT_TOKEN) {
 const token = process.env.AWS_GIT_TOKEN;
 
 //url for triggering action
-const gitBuildUrl = 'https://api.github.com/repos/PROJ-A2022-G06-AWS-2-Cloud-Organization/PROJ-A2022-G06-AWS-2-Cloud/actions/workflows/github-actions-aws-cdk-deploy.yml/dispatches';
+//const gitBuildUrl = 'https://api.github.com/repos/PROJ-A2022-G06-AWS-2-Cloud-Organization/PROJ-A2022-G06-AWS-2-Cloud/actions/workflows/github-actions-aws-cdk-deploy.yml/dispatches';
+const mockBuildUrl = 'https://api.github.com/repos/PROJ-A2022-G06-AWS-2-Cloud-Organization/PROJ-A2022-G06-AWS-2-Cloud/actions/workflows/mock-deploy.yml/dispatches';
 
 const app = express();
 const port = 8080;
@@ -31,9 +33,27 @@ app.get('/api/list', async (req, res) => {
   res.json(json_response);
 });
 
-async function triggerBuild(options) {
-  const response = await fetch(gitBuildUrl, options);
+async function triggerBuild(buildUrl, options) {
+  const response = await fetch(buildUrl, options);
+  console.log(response);
   return response;
+}
+
+async function getStatus(){
+  // url and query string for getting workflow runs
+  const timeStamp = new Date().toISOString().substring(0, 10);
+  const queryString = `?created=${timeStamp}`;
+  const getWorkFlowsUrl = `https://api.github.com/repos/PROJ-A2022-G06-AWS-2-Cloud-Organization/PROJ-A2022-G06-AWS-2-Cloud/actions/runs${queryString}`;
+
+  console.log('This is status log');
+  const workflowRunHeaders = {'Accept' : 'application/vnd.github+json', 'Authorization' : `Bearer ${ token}`};
+  
+  const workflowRuns = await fetch(getWorkFlowsUrl, {headers: workflowRunHeaders});
+  const jsonData = await workflowRuns.json();
+  const jobs_url = jsonData.workflow_runs[0].jobs_url;
+  const jobs = await fetch(jobs_url, {headers: workflowRunHeaders});
+  const jobsData = await jobs.json();
+  console.log(jobsData);
 }
 
 app.post('/api/build', async (req, res) => {
@@ -48,21 +68,32 @@ app.post('/api/build', async (req, res) => {
       'Authorization': `Bearer ${ token}`
     },
     body: JSON.stringify({
-      ref: packageName,
+      ref: 'main',
       inputs: packageParams
     })
   };
-  
-  const response = await indexFactory.triggerBuild(options); 
+  //let response = await indexFactory.triggerBuild(options); 
+  const response = await indexFactory.triggerBuild(mockBuildUrl, options);
+
   if (response.status === 204) {
     res.status(200);
-  } else {
-    res.status(response.status);
+
+  res.json(`(PLACEHOLDER) Building ${packageName} - Status: ${response.status}`);
+  //get status
+  //timeout because action is in queue for couple seconds
+  //if we get status before action is in process, we get the data from previous workflow run
+    setTimeout(() => {
+     getStatus();
+      
+    }, 5000);
+    
   }
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(`(PLACEHOLDER) Building ${ packageName } - Status: ${ response.status}`);
 });
+
+
+  
+  //let run_id = JSON.parse(workflowRuns).workflow_runs[0].id;
+  //console.log(run_id);
 
 app.post('/api/auth', async (req, res) => {
   const json = req.body;
@@ -81,11 +112,11 @@ app.post('/api/auth', async (req, res) => {
   if (users.includes(username)) {
     res.status(200);
   } else {
-    res.status(404);
-  }
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json('Data: no-data');
+      res.status(404);
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json('Data: no-data');
 });
 
 app.listen(port, () => {
